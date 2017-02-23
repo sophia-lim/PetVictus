2aed0bea7bcf9238a780ff925c7d0b932046bf97
Sophia L <Sophia L>
2017-02-22 19:17:52 -0500

    Implemented mobile device camera and scene changing

---
+++ Assets/Scripts/phoneCamera.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class phoneCamera : MonoBehaviour {
+
+    private bool camAvailable;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private void Start() {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0) {
+            Debug.Log("No camera detected on mobile.");
+            camAvailable = false;
+            return;
+        }
+
+        //Go through array of cameras available on device
+        for (int i = 0; i < devices.Length; i++) {
+            if(!devices[i].isFrontFacing) {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        //Unable to find back-facing camera
+        if (backCam == null) {
+            Debug.Log("Unable to find a back camera");
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvailable = true;
+    }
+
+    private void Update() {
+        if (!camAvailable) {
+            return;
+        }
+
+        float ratio = (float)backCam.width / (float)backCam.height;
+        fit.aspectRatio = ratio;
+
+        // If it is on a mirror side vertically, it is going to equal -1, if not it will equal 1
+        float scaleY = backCam.videoVerticallyMirrored ? -1f: 1f;
+
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        //Proper angle
+
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+
+}
