628d9f4fcc29b17c8a2f8baa9de293439f9121da
Sophia L <Sophia L>
2017-03-06 03:34:05 -0500

    Wireframe, changed fonts, added UI scripts

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -16,9 +16,24 @@ public class menu : MonoBehaviour {
     }
 
     public void StartGame() {
+        SceneManager.LoadScene("post_create");
+    }
+
+    public void Registration() {
+        SceneManager.LoadScene("registration");
+    }
+
+    public void Login() {
         SceneManager.LoadScene("login");
     }
 
+    public void Information() {
+        SceneManager.LoadScene("information");
+    }
+
+    public void PostEdit() {
+        SceneManager.LoadScene("post_edit");
+    }
 
     public void ExitGame() {
         Application.Quit();
