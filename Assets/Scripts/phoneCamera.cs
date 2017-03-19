371a88e43a6f34014d0927c709de296215f0af36
Sophia L <Sophia L>
2017-03-19 16:48:52 -0400

    Issues with picture textures, added logo

--- Assets/Scripts/phoneCamera.cs
+++ Assets/Scripts/phoneCamera.cs
@@ -5,9 +5,13 @@ using UnityEngine.UI;
 
 public class phoneCamera : MonoBehaviour {
 
+    public string path = "";
     private bool camAvailable;
     private WebCamTexture backCam;
     private Texture defaultBackground;
+
+    public Material savedImageMaterial;
+    public Texture savedImageTexture;
 
     public RawImage background;
     public AspectRatioFitter fit;
@@ -55,9 +59,30 @@ public class phoneCamera : MonoBehaviour {
         background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
 
         //Proper angle: Not sure I need it for the app since it's going to be square
-        //
-        //int orient = -backCam.videoRotationAngle;
-        //background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
     }
 
+    //Returns filename modified with date and time
+    string fileName(int width, int height) {
+        return string.Format("screen_{0}x{1}_{2}.png",
+                              width, height,
+                              System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void takePicture() {
+        Application.CaptureScreenshot(Application.persistentDataPath + "/Resources/" + fileName(1440, 2560));
+        Debug.Log(Application.persistentDataPath + "/Resources/" + fileName(1440, 2560));
+        createNewTexture();
+    }
+
+    private void createNewTexture() {
+        savedImageTexture = Resources.Load(fileName(1440, 2560)) as Texture;
+        gameObject.GetComponent<Renderer>().material.mainTexture = savedImageTexture;
+    }
+
+    public void viewPicture() {
+        
+    }
 }
