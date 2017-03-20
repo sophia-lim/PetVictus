c75c92a3ef36f9a0d3e1f4f911dad449a3a118c7
Sophia L <Sophia L>
2017-03-19 21:24:01 -0400

    Installed Gallery Screenshot plugin, by Second Fury

--- Assets/Scripts/mealRawImage.cs
+++ Assets/Scripts/mealRawImage.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mealRawImage : MonoBehaviour {
     public Material savedImageMaterial;
     public Texture savedImageTexture;
+
+    public bool creatingFile = false;
+    public string captureButton = "captureButton";
+    public Text path;
+    public string stringPath;
 
     // Use this for initialization
     void Start () {
@@ -13,23 +20,29 @@ public class mealRawImage : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//if (creatingFile) {
+        //    string origin = System.IO.Path.Combine(Application.persistentDataPath, fileName());
+        //    string destination = ""
+        //}
 	}
 
     //Returns filename modified with date and time
-    string fileName(int width, int height) {
-        return string.Format("screen_{0}x{1}_{2}.png",
-                              width, height,
+    string fileName() {
+        return string.Format("/screen_{0}.png",
                               System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
     }
 
     public void takePicture() {
-        Application.CaptureScreenshot(Application.persistentDataPath + "/Resources/" + fileName(1440, 2560));
-        Debug.Log(Application.persistentDataPath + "/Resources/" + fileName(1440, 2560));
+        Application.CaptureScreenshot(Application.persistentDataPath + fileName());
+        //creatingFile = true;
+        Debug.Log(Application.persistentDataPath);
+        Debug.Log(Application.persistentDataPath + fileName());
+        stringPath = Application.persistentDataPath + fileName();
+        stringPath = path.text;
     }
 
     private void createNewTexture() {
-        savedImageTexture = Resources.Load(fileName(1440, 2560)) as Texture;
+        //savedImageTexture = Resources.Load(fileName(1440, 2560)) as Texture;
         //gameObject.GetComponent<Renderer>().material.mainTexture = savedImageTexture;
     }
 }
