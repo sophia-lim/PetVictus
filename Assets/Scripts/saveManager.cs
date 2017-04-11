b864e8c8c30ce2189e1fabfe514b1b7df941bf9b
Sophia L <Sophia L>
2017-04-11 18:04:49 -0400

    State management to local phone, and buttons to tag menu

---
+++ Assets/Scripts/saveManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saveManager : MonoBehaviour {
+
+    // So you can access the save state anywhere in the game or when you want to quickly save on the go.
+    public static saveManager Instance { set; get; }
+    public saveState state;
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+        Load();
+    }
+    
+    // Save the whole state of this saveState script to the player pref
+    public void Save() {
+        //Serialize Save State
+        PlayerPrefs.SetString("save", helper.Serialize<saveState>(state));
+    }
+
+    // Load the previous save state from the player prefs
+    public void Load() {
+        if (PlayerPrefs.HasKey("save")) {
+            // Deserialize Save State
+            state = helper.Deserialize<saveState>(PlayerPrefs.GetString("save"));
+            Debug.Log("State loaded");
+
+        } else {
+            state = new saveState();
+            Save();
+            Debug.Log("No Save File found creating a new one");
+        }
+    }
+    
+    /*Attempt buying equipment, return true/ false 
+    public bool BuyEquip(int index, int cost) {
+        if (state.gold >= cost) {
+            //enough money, remove the amount from the current gold stack.
+            state.gold -= cost;
+            UnlockEquipment(index);
+
+            // Save progress & changes made.
+            Save();
+
+            return true;
+        } else {
+            // Not enough monwy, return false
+            return false;
+        }
+    }*/
+
+    public bool confirmPostCreation() {
+
+        return true;
+    }
+    
+
+    // Reset the whole save file
+    public void ResetSave() {
+        PlayerPrefs.DeleteKey("save");
+    }
+}
---
+++ Assets/Scripts/saveState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+public class saveState {
+
+    // Count of food group per post
+    public int postVeggieCount = 0;
+    public int postMeatCount = 0;
+    public int postSeafoodCount = 0;
+    public int postDairyCount = 0;
+
+    // Count of food group in total (all posts)
+    public int totalVeggieCount = 0;
+    public int totalMeatCount = 0;
+    public int totalSeafoodCount = 0;
+    public int totalDairyCount = 0;
+    
+    // List of moods possibilities
+    // 0: Ecstatic   || 4: Alert
+    // 1: Happy      || 5: Sick
+    // 2: Content    || 6: Unhappy
+    // 3: Worried    || 7: Angry
+    ArrayList moods = new ArrayList { "Ecstatic", "Happy", "Content", "Worried", "Alerted", "Sick", "Unhappy", "Angry"};
+
+    // Current mood
+    public string currentMood = "Content";
+    public int currentMoodIndex = 2;
+}
